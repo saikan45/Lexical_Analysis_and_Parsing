1062805e6e53616d396c955eaca0f67790862816
Suman Mia (Saikan) <sumanmia.cse@gmail.com>
2023-06-24 19:13:16 +0600

    Add files via upload

---
+++ Keyword Identifier/input.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+//this is a comment
+int main()
+{
+    int number = 10;
+    double value = 1.67;
+    printf("The number is: %d\n", number);
+    return 0;
+}
+/*
+this is a multi line comment
+*/
